a7185bc779094eb62e3a6468867c0b80d8cf4ce6
Bisquitman <lynx8309@gmail.com>
2023-11-23 08:46:44 +0300

    refactoring: handleClients

--- index.js
+++ index.js
@@ -1,64 +1,62 @@
-import http from "node:http";
-import fs from "node:fs/promises";
-import { sendError } from "./modules/send.js";
-import { checkFileExist, createFileIfNotExist } from "./modules/checkFile.js";
-import { handleComediansRequest } from "./modules/handleComediansRequest.js";
-import { handleAddClient } from "./modules/handleAddClient.js";
-import { handleClientsRequest } from "./modules/handleClientsRequest.js";
-import {handleUpdateClient} from "./modules/handleUpdateClient.js";
+import http from 'node:http';
+import fs from 'node:fs/promises';
+import { sendError } from './modules/send.js';
+import { checkFileExist, createFileIfNotExist } from './modules/checkFile.js';
+import { handleComediansRequest } from './modules/handleComediansRequest.js';
+import { handleAddClient, handleClientsRequest, handleUpdateClient } from './modules/handleClients.js';
 
 const PORT = 8080;
-const COMEDIANS = "./comedians.json";
-export const CLIENTS = "./clients.json";
+const COMEDIANS = './comedians.json';
+export const CLIENTS = './clients.json';
 
 const startServer = async (port) => {
   if (!(await checkFileExist(COMEDIANS))) return;
   await createFileIfNotExist(CLIENTS);
 
-  const comediansData = await fs.readFile(COMEDIANS, "utf-8");
+  const comediansData = await fs.readFile(COMEDIANS, 'utf-8');
   const comedians = JSON.parse(comediansData);
 
   const server = http.createServer(async (request, response) => {
     try {
-      response.setHeader("Access-Control-Allow-Origin", "*");
+      response.setHeader('Access-Control-Allow-Origin', '*');
 
-      const segments = request.url.split("/").filter(Boolean); // Получить все, кроме первого
-      console.log("segments: ", segments);
+      const segments = request.url.split('/').filter(Boolean); // Получить все, кроме первого
+      console.log('segments: ', segments);
 
       if (!segments.length) {
-        sendError(response, 404, "Страница не найдена");
+        sendError(response, 404, 'Страница не найдена');
       }
 
-      // Деструктуризайия segments: segments[0] === resource
+      // Деструктуризация segments: segments[0] === resource
       //                            segments[1] === id
       const [resource, id] = segments;
 
-      if (request.method === "GET" && resource === "comedians") {
+      if (request.method === 'GET' && resource === 'comedians') {
         handleComediansRequest(request, response, comedians, id);
         return;
       }
 
-      if (request.method === "POST" && resource === "clients") {
+      if (request.method === 'POST' && resource === 'clients') {
         // POST '/clients'
         // Добавление клиента
         handleAddClient(request, response);
         return;
       }
 
-      if (request.method === "GET" && resource === "clients" && id) {
+      if (request.method === 'GET' && resource === 'clients' && id) {
         // GET '/clients/:ticket'
         // Получение клиента по номеру билета
         await handleClientsRequest(request, response, id);
         return;
       }
 
-      if (request.method === "PATCH" && resource === "clients" && id) {
+      if (request.method === 'PATCH' && resource === 'clients' && id) {
         // PATCH '/clients/:ticket'
         // Изменение данных клиента по номеру билета
         handleUpdateClient(request, response, id);
         return;
       }
-      sendError(response, 404, "Страница не найдена");
+      sendError(response, 404, 'Страница не найдена');
     } catch (error) {
       sendError(response, 500, `Ошибка сервера: ${error}`);
     }
@@ -66,9 +64,11 @@ const startServer = async (port) => {
   server.listen(port, () => {
     console.log(`Сервер запущен и доступен по адресу http://localhost:${port}`);
   });
-  server.on("error", (err) => {
-    if (err.code === "EADDRINUSE") {
-      console.log(`Порт ${port} занят. Пробуем запуститься на порту ${port + 1}`);
+  server.on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+      console.log(
+        `Порт ${port} занят. Пробуем запуститься на порту ${port + 1}`,
+      );
       startServer(port + 1);
     } else {
       console.log(`Возникла ошибка: ${err}`);
---
+++ modules/handleClients.js
@@ -0,0 +1,105 @@
+import fs from "node:fs/promises";
+import {createFileIfNotExist} from "./checkFile.js";
+import {CLIENTS} from "../index.js";
+import {sendError, sendResponse} from "./send.js";
+import {readRequestBody} from "./helpers.js";
+
+export const handleAddClient = async (request, response) => {
+  try {
+    const body = await readRequestBody(request);
+    const newClient = JSON.parse(body);
+
+    if (!newClient.fullName || !newClient.phone || !newClient.ticketNumber || !newClient.booking) {
+      sendError(response, 400, "Неверные данные клиента");
+      return;
+    }
+
+    if (
+      newClient.booking &&
+      (!newClient.booking.length ||
+        !Array.isArray(newClient.booking) ||
+        !newClient.booking.every((item) => item.comediant && item.time))
+    ) {
+      sendError(response, 400, "Неверно заполнены поля бронирования");
+      return;
+    }
+
+    await createFileIfNotExist(CLIENTS);
+    const clientData = await fs.readFile(CLIENTS, "utf-8");
+    const clients = JSON.parse(clientData);
+
+    clients.push(newClient);
+    await createFileIfNotExist(CLIENTS);
+    await fs.writeFile(CLIENTS, JSON.stringify(clients));
+
+    sendResponse(response, newClient);
+  } catch (error) {
+    console.log("error: ", error);
+    sendError(response, 500, `Ошибка сервера при чтении запроса`);
+  }
+};
+
+export const handleUpdateClient = async (request, response, ticketNumber) => {
+  try {
+    const body = await readRequestBody(request);
+    const updateDataClient = JSON.parse(body);
+
+    if (!updateDataClient.fullName || !updateDataClient.phone || !updateDataClient.ticketNumber || !updateDataClient.booking) {
+      sendError(response, 400, "Неверные данные клиента");
+      return;
+    }
+
+    if (
+      updateDataClient.booking &&
+      (!updateDataClient.booking.length ||
+        !Array.isArray(updateDataClient.booking) ||
+        !updateDataClient.booking.every((item) => item.comediant && item.time))
+    ) {
+      sendError(response, 400, "Неверно заполнены поля бронирования");
+      return;
+    }
+
+    await createFileIfNotExist(CLIENTS);
+    const clientData = await fs.readFile(CLIENTS, "utf-8");
+    const clients = JSON.parse(clientData);
+
+    const clientIndex = clients.findIndex((c) => (c.ticketNumber = ticketNumber));
+
+    if (clientIndex === -1) {
+      sendError(response, 404, `Клиент с билетом № ${ticketNumber} не найден`);
+    }
+
+    clients[clientIndex] = {
+      ...clients[clientIndex],
+      ...updateDataClient,
+    };
+
+    await createFileIfNotExist(CLIENTS);
+    await fs.writeFile(CLIENTS, JSON.stringify(clients));
+    sendResponse(response, clients[clientIndex]);
+  } catch (error) {
+    console.error(`error: ${error}`);
+    sendError(response, 500, `Ошибка сервера при обновлении данных`);
+  }
+};
+
+export const handleClientsRequest = async (request, response, ticketNumber) => {
+  try {
+    await createFileIfNotExist(CLIENTS);
+
+    const clientData = await fs.readFile(CLIENTS, "utf-8");
+    const clients = JSON.parse(clientData);
+    const client = clients.find((c) => c.ticketNumber === ticketNumber);
+
+    if (!client) {
+      sendError(response, 404, `Клиент с билетом № ${ticketNumber} не найден`);
+      return;
+    }
+
+    sendResponse(response, client);
+  } catch (error) {
+    console.error(`Ошибка при обработке запроса: ${error}`);
+    sendError(response, 500, `Ошибка сервера при обработке запроса: ${error}`);
+  }
+};
+
